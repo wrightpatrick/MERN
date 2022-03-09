a3721334f0c00995ba726c94aa94a33cfb4269f7
Patrick Wright <patrickdane@me.com>
2022-03-09 12:16:13 -0500

    first jsx core

---
+++ React/app/src/App.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello Dojo!</h1>
+			<h3>Things I need to do:</h3>
+			<p>* Learn React</p>
+			<p>* Climb Mt. Everest</p>
+			<p>* Run a marathon</p>
+			<p>* Feed the dogs</p>
+		</div>
+	);
+}
+
+export default App;
