17ba7f2e6b4cd1e752851500ab1c404b89d4b518
Patrick Wright <patrickdane@me.com>
2022-03-13 16:47:24 -0400

    putting it together

--- React/personcard/src/App.js
+++ React/personcard/src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import PersonCard from './components/PersonCard';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hey, you're pretty good!</h1>
-	  <PersonCard firstName="Iriqois" lastName="Pliskin" age={35} hairColor="Brown"/>
-	  <PersonCard firstName="Revolver" lastName="Ocelot" age={52} hairColor="Brown"/>
-	  <PersonCard firstName="Grey" lastName="Fox" age={42} hairColor="Grey"/>
-	  <PersonCard firstName="Hal" lastName="Otacon" age={33} hairColor="Brown"/>
-    </div>
-  );
+	
+	return (
+		<div className="App">
+		<h1>Hey, you're pretty good!</h1>
+		<PersonCard firstName="Iriqois" lastName="Pliskin" age={35} hairColor="Brown"/>
+		<PersonCard firstName="Revolver" lastName="Ocelot" age={52} hairColor="Brown"/>
+		<PersonCard firstName="Grey" lastName="Fox" age={42} hairColor="Grey"/>
+		<PersonCard firstName="Hal" lastName="Otacon" age={33} hairColor="Brown"/>
+	</div>
+	);
 }
 
 export default App;
--- React/personcard/src/components/PersonCard.jsx
+++ React/personcard/src/components/PersonCard.jsx
@@ -3,6 +3,16 @@ import React, {Component} from "react";
 class PersonCard extends Component {
 	constructor(props){
 		super(props);
+		this.state = {
+			age: 0
+		}
+	}
+	ageUp = () => {
+		this.setState({age: this.state.age + 1});
+	}
+	birthdayButton = () => {
+		console.log("button clicked");
+		
 	}
 
 	render(){
@@ -10,8 +20,12 @@ class PersonCard extends Component {
 			<div className="card" style={{width: "18rem"}}>
 				<div className="card-body m-2">
 					<h2 className="card-title">{this.props.lastName}, {this.props.firstName}</h2>
+					<p className="card-text">Age: {this.state.age}</p>
 					<p className="card-text">Age: {this.props.age}</p>
 					<p className="card-text">Hair Color: {this.props.hairColor}</p>
+					<button className="btn btn-outline-success" onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
+					<button className="btn btn-outline-success" onClick={ this.ageUp }>State Age Up</button>
+					<button className="btn btn-outline-info" onClick={ this.birthdayButton }>Props Age Up</button>
 				</div>
 			</div>
 		)
